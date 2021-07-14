6143405124c833c75c3c816f45dcd0dbe88eecdf
Minh Phan <phanmn1@Minhs-MacBook-Pro.local>
2021-07-13 22:43:27 -0700

    Section 4 Done

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,6 +1,9 @@
 import ExpenseItem from './ExpenseItem'
+import ExpensesFilter from './ExpensesFilter'
 import Card from '../UI/Card'
 import './Expenses.css'
+import { useState } from 'react'
+
 function Expenses(props) {
     const expensesList = props.expenses.map(expense => {
         return <ExpenseItem title={expense.title}
@@ -8,8 +11,20 @@ function Expenses(props) {
                             date={expense.date}/>
     })
 
+    const [filteredYear, setFilteredYear] = useState('2020')
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear)
+    }
+
     return(
-        <Card className="expenses">{expensesList}</Card>
+        <div>
+            
+            <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                {expensesList}
+            </Card>
+        </div>
     )
 }
 
