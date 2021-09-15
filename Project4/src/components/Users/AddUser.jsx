670b32e1f8cdad86ac8c71921eb62d8996811bb9
Minh Phan <mpflamingmarshmallow@gmail.com>
2021-09-14 18:35:24 -0700

    Adding a users list component

--- Project4/src/components/Users/AddUser.jsx
+++ Project4/src/components/Users/AddUser.jsx
@@ -1,17 +1,45 @@
+import { useState } from 'react'
+import Card from '../UI/Card'
+import Button from '../UI/Button'
+import classes from './AddUser.module.css'
 
 const AddUser = props => {
 
+    const [enteredUsername, setEnteredUsername] = useState('')
+    const [enteredAge, setEnteredAge] = useState('')
+
     const AddUserHandler = (event) => {
         event.preventDefault()
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0)
+            return 
+
+        if(+enteredAge < 1) {
+            return
+        }
+        console.log(enteredUsername, enteredAge)
+        setEnteredUsername('')
+        setEnteredAge('')
     }
 
-    return(<form onSubmit={AddUserHandler}>
-        <label htmlFor='username'>Username</label>
-        <input id='username' type='text'/>
-        <label htmlFor='age'>Age(Years)</label>
-        <input id='age' type='number'/>
-        <button type='submit'>Add User</button>
-    </form>)
+    const usernameChangeHandler  = (event) => {
+        setEnteredUsername(event.target.value)
+    }
+
+    const ageChangeHandler  = (event) => {
+        setEnteredAge(event.target.value)
+    }
+
+    return(
+        <Card className={classes.input}>
+            <form onSubmit={AddUserHandler}>
+                <label htmlFor='username'>Username</label>
+                <input id='username' type='text' value={enteredUsername} onChange={usernameChangeHandler}/>
+                <label htmlFor='age'>Age(Years)</label>
+                <input id='age' type='number' value={enteredAge} onChange={ageChangeHandler}/>
+                <Button type='submit'>Add User</Button>
+            </form>
+        </Card>
+    )
 }
 
 export default AddUser
