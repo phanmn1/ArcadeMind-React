5b0e05465195f97791d6d928a7469eb38fc727bd
Minh Phan <mpflamingmarshmallow@gmail.com>
2021-09-15 18:31:42 -0700

    React Fragments explanation

--- Project4/src/components/Users/AddUser.jsx
+++ Project4/src/components/Users/AddUser.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Card from '../UI/Card'
 import Button from '../UI/Button'
 import classes from './AddUser.module.css'
 import ErrorModal from '../UI/ErrorModal'
+import Wrapper from '../Helpers/Wrapper'
 
 const AddUser = props => {
 
@@ -46,7 +47,7 @@ const AddUser = props => {
         setError(null)
     }
 
-    return(<>
+    return(<React.Fragment>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className={classes.input}>
                 <form onSubmit={AddUserHandler}>
@@ -57,7 +58,7 @@ const AddUser = props => {
                     <Button type='submit'>Add User</Button>
                 </form>
             </Card>
-        </>
+        </React.Fragment>
     )
 }
 
