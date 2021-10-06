66f890715d5f9f33fe9afaa4bcf5abf5ff8ca2d4
Minh Phan <mpflamingmarshmallow@gmail.com>
2021-10-06 14:40:46 -0700

    Cart provider reducer

--- FoodOrderApp/src/store/CartProvider.js
+++ FoodOrderApp/src/store/CartProvider.js
@@ -1,16 +1,38 @@
+import { useReducer } from 'react'
 import CartContext from './cart-context'
 
+const defaultCartState = {
+    items: [], 
+    totalAmount: 0
+}
 
+const cartReducer = (state, action) => {
+    if(action.type === 'ADD') {
+        const updatedItems = state.items.concat(action.item)
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+
+    return defaultCartState
+}
 
 const CartProvider = props => {
 
-    const addItemToCarthandler = (item) => {}
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
+    const addItemToCarthandler = (item) => {
+        dispatchCartAction({type: 'ADD', item})
+    }
 
-    const removeItemFromCartHandler = (id) => {}
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({type: 'REMOVE', id})
+    }
 
     const cartContext = {
-        items: [], 
-        totalAmount: 0, 
+        items: cartState.items, 
+        totalAmount: cartState.totalAmount, 
         addItem: addItemToCarthandler, 
         removeItem: removeItemFromCartHandler
     }
