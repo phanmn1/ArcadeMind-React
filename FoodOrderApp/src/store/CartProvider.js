b4871d825b5f24178715708e1a366c8dbf182aa3
Minh Phan <mpflamingmarshmallow@gmail.com>
2021-10-20 18:00:03 -0700

    Added more complex render logic

--- FoodOrderApp/src/store/CartProvider.js
+++ FoodOrderApp/src/store/CartProvider.js
@@ -8,8 +8,23 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if(action.type === 'ADD') {
-        const updatedItems = state.items.concat(action.item)
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+        const existingcartItemIndex = state.items.findIndex(item => item.id === action.item.id)
+        const existingCartItem = state.items[existingcartItemIndex]
+        let updatedItems
+        
+        if(existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            }
+
+            updatedItems = [...state.items]
+            updatedItems[existingcartItemIndex] = updatedItem
+        } else {
+            updatedItems = state.items.concat(action.item)
+        }
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
