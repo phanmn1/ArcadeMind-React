b54af1e4d70ceaef6e12c6848c72659fd739863d
Minh Phan <phanmn1@Minhs-MacBook-Pro.local>
2021-10-27 19:54:41 -0700

    Class based components

--- ClassComponents/src/components/Users.js
+++ ClassComponents/src/components/Users.js
@@ -1,14 +1,50 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
 
+
+class Users extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      showUsers: true
+    }
+  }
+
+  
+  
+  toggleUsersHandler() {
+    // this.state.showUsers = false // NOT how to do it
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers }
+    })
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+/*
 const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
@@ -32,6 +68,6 @@ const Users = () => {
       {showUsers && usersList}
     </div>
   );
-};
+}; */
 
 export default Users;
