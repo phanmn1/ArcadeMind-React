40be62d9aed278a1c310d06c9e54e9c6916706e2
Minh Phan <mpflamingmarshmallow@gmail.com>
2021-10-29 21:15:38 -0700

    Custom hooks http

--- CustomHooks-Real/src/App.js
+++ CustomHooks-Real/src/App.js
@@ -3,41 +3,29 @@ import React, { useEffect, useState } from 'react';
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
 
+import useHttp from './hooks/use-http';
+
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-de08e-default-rtdb.firebaseio.com/tasks.json'
-      );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp()
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
-
+  // When checking use effect dependencies, make sure that the dependencies 
+  // are not other functions or objects. If they are functions then useCallback
+  // if they are objects then memo or refactor to put the object in the useEffect 
+  // hook
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = tasksObj => {
+      const loadedTasks = [];
+  
+        for (const taskKey in tasksObj) {
+          loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+        }
+  
+        setTasks(loadedTasks);
+    }
+    fetchTasks({ url: 'https://react-http-de08e-default-rtdb.firebaseio.com/tasks.json'}, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
