0a335b9c5c2de03524898f273f40511dd22d63d9
Minh Phan <mpflamingmarshmallow@gmail.com>
2022-02-21 12:01:24 -1000

    Sorted quotes

--- React-Router-Project/src/pages/QuoteDetail.jsx
+++ React-Router-Project/src/pages/QuoteDetail.jsx
@@ -1,13 +1,25 @@
 import { Fragment } from 'react'
 import { useParams, Route } from 'react-router-dom'
+import HighlightedQuote from '../components/quotes/HighlightedQuote'
 import Comments from '../components/comments/Comments'
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Max', text: 'Learning React is fun!'},
+    { id: 'q2', author: 'Maximillian', text: 'Learning react is great'}
+]
 
 const QuoteDetail = () => {
     const params = useParams()
+
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+
+    if(!quote) {
+        return <p>No Quote Found</p>
+    }
+
     return <Fragment>
-            <h1>Quote Detail</h1>
-            <p>{params.quoteId}</p>
-            <Route path={`/quotes/${params.quoteId}/comments`}>
+            <HighlightedQuote text={quote.text} author={quote.author}/>
+            <Route path='/quotes/:quoteId/comments'>
                 <Comments/>
             </Route>
         </Fragment>
